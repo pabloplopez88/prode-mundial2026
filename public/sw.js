50cf8a6371fb4b73a6fb75a5a2ca1b29d85126fe
Pablo López <46566986+pabloplopez88@users.noreply.github.com>
2026-06-03 12:16:32 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,24 @@
+const CACHE_NAME = 'prode-v1'
 
+self.addEventListener('install', () => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', (e) => {
+  if (!e.request.url.startsWith(self.location.origin)) return
+  e.respondWith(
+    fetch(e.request).then(response => {
+      const clone = response.clone()
+      caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone))
+      return response
+    }).catch(() => caches.match(e.request))
+  )
+})
